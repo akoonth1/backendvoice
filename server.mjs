97dbce855bf6133d4183dca8906c0537d9bc660f
akoonth1 <a.koontharana@gmail.com>
2025-08-22 19:35:27 -0400

    userSchema

--- server.mjs
+++ server.mjs
@@ -1,8 +1,16 @@
 
 import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 const app = express()
 const port = 3000
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
